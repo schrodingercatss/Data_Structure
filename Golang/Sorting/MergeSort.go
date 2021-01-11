dae906bbf8c944e8a395dd6a172e8eecae88e39b
量子态薛定谔 <maxmalmap@qq.com>
2021-01-11 18:26:44 +0800

    Sorting

--- Golang/Sorting/MergeSort.go
+++ Golang/Sorting/MergeSort.go
@@ -1,1 +1,28 @@
 package Sorting
+
+func MergeSort(arr []int) []int {
+	n := len(arr)
+	if n <= 1 {
+		return arr
+	}
+	mid := n / 2
+	left := MergeSort(arr[:mid])
+	right := MergeSort(arr[mid:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) (res []int) {
+	l, r := 0, 0
+	for l < len(left) && r < len(right) {
+		if left[l] < right[r] {
+			res = append(res, left[l])
+			l++
+		} else {
+			res = append(res, right[r])
+			r++
+		}
+	}
+	res = append(res, left[l:]...)
+	res = append(res, right[r:]...)
+	return
+}
--- Golang/Sorting/QuickSort.go
+++ Golang/Sorting/QuickSort.go
@@ -1,1 +1,22 @@
 package Sorting
+
+func QuickSort(arr []int, l, r int) {
+	if l >= r {
+		return
+	}
+	x := arr[l]
+	i, j := l, r
+	for i < j {
+		for arr[i] < x {
+			i++
+		}
+		for arr[j] > x {
+			j--
+		}
+		if i < j {
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	QuickSort(arr, l, j)
+	QuickSort(arr, j+1, r)
+}
